7c13876fd0d5ae1fec2a423f502c163b573fcecf
shaunhwq <48981455+shaunhwq@users.noreply.github.com>
2022-11-30 15:28:55 +0800

    Add main check

--- fhdr_demo.py
+++ fhdr_demo.py
@@ -25,34 +25,35 @@ def get_model_input(image: np.array, device: str) -> torch.tensor:
     return image
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_dir", type=str, required=True)
-parser.add_argument("--output_dir", type=str, required=True)
-parser.add_argument("--iter_num", type=int, required=True)
-parser.add_argument("--device", type=str, default="cuda:0")
-parser.add_argument("--checkpoint_dir", type=str, default="checkpoints")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--iter_num", type=int, required=True)
+    parser.add_argument("--device", type=str, default="cuda:0")
+    parser.add_argument("--checkpoint_dir", type=str, default="checkpoints")
+    args = parser.parse_args()
 
-os.makedirs(args.output_dir, exist_ok=True)
-checkpoint_path = os.path.join(args.checkpoint_dir, f"FHDR-iter-{args.iter_num}.ckpt")
-assert os.path.exists(checkpoint_path), "Unable to find pretrained weights"
-    
-model = FHDR(iteration_count=args.iter_num)
-model.load_state_dict(torch.load(checkpoint_path))
-model.to(args.device)
-model.eval()
+    os.makedirs(args.output_dir, exist_ok=True)
+    checkpoint_path = os.path.join(args.checkpoint_dir, f"FHDR-iter-{args.iter_num}.ckpt")
+    assert os.path.exists(checkpoint_path), "Unable to find pretrained weights"
 
-image_paths = [os.path.join(args.input_dir, img_path) for img_path in os.listdir(args.input_dir) if img_path[0] != "."]
+    model = FHDR(iteration_count=args.iter_num)
+    model.load_state_dict(torch.load(checkpoint_path))
+    model.to(args.device)
+    model.eval()
 
-with torch.no_grad():
-    for img_path in tqdm(image_paths, total=len(image_paths), desc=f"Running FHDR [iter-{args.iter_num}]..."):
-        in_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        model_input = get_model_input(in_image, args.device)
+    image_paths = [os.path.join(args.input_dir, img_path) for img_path in os.listdir(args.input_dir) if img_path[0] != "."]
 
-        output = model(model_input)
-        output = output[-1]
+    with torch.no_grad():
+        for img_path in tqdm(image_paths, total=len(image_paths), desc=f"Running FHDR [iter-{args.iter_num}]..."):
+            in_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+            model_input = get_model_input(in_image, args.device)
 
-        new_name = os.path.splitext(os.path.basename(img_path))[0] + ".hdr"
-        output_path = os.path.join(args.output_dir, new_name)
-        # https://github.com/mukulkhanna/FHDR/issues/7
-        util.save_hdr_image(img_tensor=output, batch=0, path=output_path)
+            output = model(model_input)
+            output = output[-1]
+
+            new_name = os.path.splitext(os.path.basename(img_path))[0] + ".hdr"
+            output_path = os.path.join(args.output_dir, new_name)
+            # https://github.com/mukulkhanna/FHDR/issues/7
+            util.save_hdr_image(img_tensor=output, batch=0, path=output_path)
